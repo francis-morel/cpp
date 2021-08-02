82209825bba54ca5e6972bbc483660df904e4b47
Francis <poste18@MJL.local>
2021-08-02 13:09:08 -0400

    test

---
+++ test1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int obtenirInt(string message, int min, int max){
+    int nombre = min - 1;
+
+    while(nombre < min || nombre > max){
+        cout << message << " (" << min << "-" << max << "): ";
+        cin >> nombre;
+        
+    }
+
+    return nombre;
+}
+
+int main(){
+    int valeur = obtenirInt("Veuillez entrer un int", 0, 100);
+
+    cout << "Voici la valeur entrée: " << valeur << endl;
+    return 0;
+}
+
